e1ce56eaea8bf9afe7b0256f0b38297d8751562f
Nethmina Sandaruwan <125788788+ByteBigBoss@users.noreply.github.com>
2023-11-02 19:22:47 +0530

    add articale read func

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center bg-slate-50">
+    <main className="flex min-h-screen flex-col items-center bg-slate-50 overflow-x-hidden">
      
      <div className='pt-6 w-full bg-black flex justify-center pb-[20px]'>
     <div className='frame-container'>
